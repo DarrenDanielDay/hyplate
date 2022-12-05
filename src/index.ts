5974da414c8c1b883cdc5ed76127b5577e220624
DarrenDanielDay <Darren_Daniel_Day@hotmail.com>
2022-12-06 01:19:00 +0800

    feat: add main exports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-export {}
+export { $, $$, after, appendChild, before, bindAttr, bindEvent, bindText, select, seqAfter } from "./core.js";
+export * from "./jsx-runtime.js";
+export * from "./directive.js";
+export {
+  useAnchor,
+  useChildView,
+  useCleanUp,
+  useCleanUpCollector,
+  useEvent,
+  useHost,
+  useParent,
+  useRef,
+} from "./hooks.js";
+
+export { query, setDiffer, source, subscribe } from "./store.js";
+export * from "./template.js";
