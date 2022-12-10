af32ce9ba00ba80cc93b8c5a9fc2d0f1bd029105
DarrenDanielDay <Darren_Daniel_Day@hotmail.com>
2022-12-10 20:13:17 +0800

    chore: add license info and `types` export

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,3 +1,10 @@
+/**
+ * @license MIT
+ * Copyright (C) 2022  DarrenDanielDay <Darren_Daniel_Day@hotmail.com>
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 import type { ParseSelector } from "typed-query-selector/parser.js";
 import { bindEvent, anchorRef, select } from "./core.js";
 import type {
@@ -18,7 +25,7 @@ export const [enterHooks, quitHooks, _resolveHooks] = scopes<Hooks>();
 const resolveHooks = (): Hooks => {
   let currentHooks = _resolveHooks();
   if (!currentHooks) {
-    throw new Error(`Invalid hook call.`);
+    throw new Error(`Invalid hook call. Hooks can only be called inside the setup function of template-based component.`);
   }
   return currentHooks;
 };
