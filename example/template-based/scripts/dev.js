dc20fd58228df2e5a7e740ea349e04260acec23b
DarrenDanielDay <Darren_Daniel_Day@hotmail.com>
2022-12-21 04:40:33 +0800

    fix(example): use `esbuild.build` in WASM

--- example/template-based/scripts/dev.js
+++ example/template-based/scripts/dev.js
@@ -3,53 +3,55 @@ import esbuild from "esbuild";
 import { readFile } from "fs/promises";
 import { resolve } from "path";
 import { emit } from "hyplate/compiler";
-esbuild.serve(
-  {
-    servedir: "public",
+/** @type {esbuild.BuildOptions} */
+const options = {
+  entryPoints: ["./src/index.tsx"],
+  //#region development mode
+  platform: "neutral",
+  define: {
+    "process.env.NODE_ENV": "'development'",
   },
-  {
-    entryPoints: ["./src/index.tsx"],
-    //#region development mode
-    platform: "neutral",
-    define: {
-      "process.env.NODE_ENV": "'development'",
-    },
-    //#endregion
-    //#region production mode
-    // platform: 'browser',
-    //#endregion
-    bundle: true,
-    sourcemap: true,
-    outdir: "public/dist",
-    plugins: [
-      {
-        name: "hyplate",
-        setup(b) {
-          const pattern = /\.template(\.js|\.html)?$/;
-          b.onResolve({ filter: pattern }, async ({ path, resolveDir }) => ({
-            path: resolve(resolveDir, path.replace(pattern, ".template.html")),
-          }));
-          b.onLoad({ filter: pattern }, async ({ path }) => {
-            try {
-              // This is the essential compiler method of hyplate. All in one!
-              await emit(path, "shadowed");
-            } catch (error) {
-              return {
-                errors: [
-                  {
-                    detail: error,
-                    text: error instanceof Error ? error.message : undefined,
-                  },
-                ],
-              };
-            }
+  //#endregion
+  //#region production mode
+  // platform: 'browser',
+  //#endregion
+  bundle: true,
+  sourcemap: true,
+  outdir: "public/dist",
+  plugins: [
+    {
+      name: "hyplate",
+      setup(b) {
+        const pattern = /\.template(\.js|\.html)?$/;
+        b.onResolve({ filter: pattern }, async ({ path, resolveDir }) => ({
+          path: resolve(resolveDir, path.replace(pattern, ".template.html")),
+        }));
+        b.onLoad({ filter: pattern }, async ({ path }) => {
+          try {
+            // This is the essential compiler method of hyplate. All in one!
+            await emit(path, "shadowed");
+          } catch (error) {
             return {
-              watchFiles: [path],
-              contents: await readFile(path.replace(pattern, ".template.js")),
+              errors: [
+                {
+                  detail: error,
+                  text: error instanceof Error ? error.message : undefined,
+                },
+              ],
             };
-          });
-        },
+          }
+          return {
+            watchFiles: [path],
+            contents: await readFile(path.replace(pattern, ".template.js")),
+          };
+        });
       },
-    ],
-  }
-);
+    },
+  ],
+};
+const isStackblitz = process.argv.includes("--for-wasm");
+if (isStackblitz) {
+  esbuild.build({ ...options, watch: true });
+} else {
+  esbuild.serve({ servedir: "public" }, options);
+}
