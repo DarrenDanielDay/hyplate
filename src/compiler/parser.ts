4ed6dbf45ea123cc8a35b3a90c75e5916ded9dda
DarrenDanielDay <Darren_Daniel_Day@hotmail.com>
2022-12-23 21:59:31 +0800

    chore: add license comments

--- src/compiler/parser.ts
+++ src/compiler/parser.ts
@@ -1,3 +1,10 @@
+/**
+ * @license MIT
+ * Copyright (C) 2022  DarrenDanielDay <Darren_Daniel_Day@hotmail.com>
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 import { IAttribute, INode, ITag, parse as parseHTML, SyntaxKind } from "html5parser";
 import { warn } from "../util.js";
 import { createLocator } from "./locator.js";
