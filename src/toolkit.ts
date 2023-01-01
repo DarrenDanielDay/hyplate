664805428826abe8e015f5c966d9a1642a8ecdac
DarrenDanielDay <Darren_Daniel_Day@hotmail.com>
2023-01-01 08:49:36 +0800

    feat(toolkit): add toolkit APIs and tests

--- src/toolkit.ts
+++ src/toolkit.ts
@@ -5,6 +5,34 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import type { Differ } from "./types.js";
+import { bindAttr, bindText, interpolation } from "./binding.js";
+import { appendChild } from "./core.js";
+import { useCleanUpCollector } from "./hooks.js";
+import type { AttributesMap, Differ, Subscribable, TextInterpolation } from "./types.js";
+import { isObject } from "./util.js";
 
 export const alwaysDifferent: Differ = () => false;
+
+export const deepDiffer: Differ = (a, b) => {
+  if (!isObject(a) || !isObject(b)) {
+    // Compare identity for cases of functions & primitives.
+    return Object.is(a, b);
+  }
+  const aKeys = Reflect.ownKeys(a);
+  const bKeys = new Set(Reflect.ownKeys(b));
+  if (aKeys.length !== bKeys.size) {
+    return false;
+  }
+  return aKeys.every((key) => bKeys.has(key) && deepDiffer(Reflect.get(a, key), Reflect.get(b, key)));
+};
+
+export const useBind = <E extends Element>(el: E) => {
+  const registerCleanUp = useCleanUpCollector();
+  return {
+    text: (subscribable: Subscribable<TextInterpolation>) => registerCleanUp(bindText(el, subscribable)),
+    attr: <P extends keyof AttributesMap<E>>(name: P, subscribable: Subscribable<AttributesMap<E>[P]>) =>
+      registerCleanUp(bindAttr(el, name, subscribable)),
+    content: (fragments: TemplateStringsArray, ...bindings: (TextInterpolation | Subscribable<TextInterpolation>)[]) =>
+      registerCleanUp(interpolation(fragments, ...bindings)(appendChild(el))),
+  };
+};
---
+++ tests/toolkit.spec.ts
@@ -0,0 +1,63 @@
+import { element } from "../dist/core";
+import { alwaysDifferent, deepDiffer, useBind } from "../dist/toolkit";
+// @ts-expect-error internal exports
+import { enterHooks, quitHooks } from "../dist/hooks";
+import type { CleanUpFunc } from "../dist/types";
+import { source } from "../dist/store";
+import { noop } from "../dist/util";
+import { setHyplateStore } from "./configure-store";
+import { resetBinding } from "../dist/binding";
+describe("toolkit.ts", () => {
+  describe("always different", () => {
+    it("should always return false", () => {
+      expect(alwaysDifferent(1, 2)).toBe(false);
+      expect(alwaysDifferent(1, 1)).toBe(false);
+      expect(alwaysDifferent(NaN, NaN)).toBe(false);
+    });
+  });
+  describe("deep differ", () => {
+    it("should compare object deep", () => {
+      expect(deepDiffer({ a: 1 }, { a: 1 })).toBe(true);
+      expect(deepDiffer({ a: 1 }, { a: 2 })).toBe(false);
+      expect(deepDiffer({ a: { b: [] } }, { a: { b: [] } })).toBe(true);
+      expect(deepDiffer({ a: { b: [] } }, { a: { b: [1] } })).toBe(false);
+      expect(deepDiffer({ a: { b: [] } }, { a: { b: [] }, c: 0 })).toBe(false);
+      expect(deepDiffer({ a: { b: [] } }, { a: { b: [1] }, c: 0 })).toBe(false);
+    });
+
+    it("should compare using `Object.is` logic", () => {
+      expect(deepDiffer(NaN, NaN)).toBe(true);
+    });
+  });
+  describe("use bind", () => {
+    beforeAll(() => {
+      setHyplateStore();
+    });
+    afterAll(() => {
+      resetBinding();
+    });
+    it("should call hooks", () => {
+      expect(() => {
+        useBind(element("div"));
+      }).toThrow(/invalid hook call/i);
+    });
+    it("should register cleanup", () => {
+      const cleanups = new Set<CleanUpFunc>();
+      const cleanUpCollector = import.meta.jest.fn<CleanUpFunc, [CleanUpFunc]>((a) => {
+        cleanups.add(a);
+        return noop;
+      });
+      enterHooks({ useCleanUpCollector: () => cleanUpCollector });
+      const el = element("div");
+      const binding = useBind(el);
+      binding.attr("id", source("aaa"));
+      expect(el.id).toBe("aaa");
+      binding.content`Using useBind().content ${0} ${source(1)}`;
+      expect(el.textContent).toBe(`Using useBind().content 0 1`);
+      binding.text(source("Now changed"));
+      expect(el.textContent).toBe("Now changed");
+      cleanups.forEach((c) => c());
+      quitHooks();
+    });
+  });
+});
