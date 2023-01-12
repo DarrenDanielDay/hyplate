72b359eab11d565b07a3040c51be7be3fc155a9c
DarrenDanielDay <Darren_Daniel_Day@hotmail.com>
2023-01-12 14:52:27 +0800

    example(count): upgrade to `0.6.1`

--- example/count/index.tsx
+++ example/count/index.tsx
@@ -1,9 +1,23 @@
 import { appendChild } from "hyplate/core";
-import { source, query } from "hyplate/store";
+import { source as ref, query as computed, enableBuiltinStore } from "hyplate/store";
+
+//#region 
+// Configure the binding source and types
+// This function should be executed once before any binding is created.
+// If you want to use other library for reactivity (such as `rxjs` and `mobx`), use the `configureBinding` API.
+enableBuiltinStore();
+declare module "hyplate/types" {
+  // Use declaration merging to configure the reactive (subscribable) data type.
+  // If you want to use `rxjs`, the code might be:
+  // export interface Subscribable<T> extends Observable<T> {}
+  export interface Subscribable<T> extends Query<T> {}
+}
+//#endregion
+
 
 const App = ({ msg }: { msg: string }) => {
-  const count = source(0);
-  const doubleCount = query(() => count.val * 2);
+  const count = ref(0);
+  const doubleCount = computed(() => count.val * 2);
   console.log("This function will only be executed once on `App` attach.");
   return (
     <div class="app">
