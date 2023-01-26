90b6c904d9fbed4da8865ead976f6e67b6fc4669
DarrenDanielDay <Darren_Daniel_Day@hotmail.com>
2023-01-26 20:38:24 +0800

    feat(jsx): `jsx-dev-runtime` for tool chain

---
+++ src/jsx-dev-runtime.ts
@@ -0,0 +1,7 @@
+import { jsx } from "./jsx-runtime.js";
+import type { JSXFactory } from "./types.js";
+
+/**
+ * This JSX factory is just for tool chain compatibility.
+ */
+export const jsxDEV: JSXFactory = (type: any, props: any) => jsx(type, props);
---
+++ tests/jsx-dev-runtime.spec.tsx
@@ -0,0 +1,14 @@
+import { element } from "../dist/core";
+import { jsxDEV } from "../dist/jsx-dev-runtime";
+import { mount, unmount } from "../dist/jsx-runtime";
+
+describe("jsx-dev-runtime.ts", () => {
+  describe("jsxDEV", () => {
+    it("should create mountable", () => {
+      const mountable = jsxDEV('p');
+      const rendered = mount(mountable, element('div'));
+      expect(rendered[1]).toBeInstanceOf(window.HTMLParagraphElement);
+      unmount(rendered);
+    });
+  });
+});
