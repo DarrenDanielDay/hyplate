aac3a845dd0b91816ff76960a1f65d651db8e751
DarrenDanielDay <Darren_Daniel_Day@hotmail.com>
2023-01-27 01:19:58 +0800

    fix(jsx-dev-runtime): re-export `Fragment`

--- src/jsx-dev-runtime.ts
+++ src/jsx-dev-runtime.ts
@@ -5,3 +5,5 @@ import type { JSXFactory } from "./types.js";
  * This JSX factory is just for tool chain compatibility.
  */
 export const jsxDEV: JSXFactory = (type: any, props: any) => jsx(type, props);
+
+export { Fragment } from "./jsx-runtime.js";
